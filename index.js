0c06c142cd84f4e6af79ea0f6b7978c24d261462
susumoa <tobiasbori@gmail.com>
2020-01-17 21:27:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => res.sendFile(`${__dirname}/index.html`))
+
+app.post("/", (req, res) => {
+  const crypto = req.body.crypto;
+  const fiat =  req.body.fiat;
+  const amount = req.body.amount;
+
+const options = {
+  url: "https://apiv2.bitcoinaverage.com/convert/global",
+  method: "GET",
+  qs: {
+    from: crypto,
+    to: fiat,
+    amount: amount
+  }
+}
+
+  request(options, (error, response, body) => {
+    const data = JSON.parse(body);
+    const price = data.price;
+
+    const currentDate = data.time;
+    res.write(`<p>The current date is ${currentDate}</p>`);
+    res.write(`<h1>${amount} ${crypto} is currently worth ${price} ${fiat}</h1>`);
+
+    res.send();
+  });
+
+})
+
+
+app.listen(3000, () => console.log("Server is running on port 3000."))
